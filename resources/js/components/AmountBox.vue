35a59832f764ba32db2bd423e4f6f118b82a5919
Asad <asadali.boson@gmail.com>
2022-11-02 17:09:03 +0500

    Changes

--- resources/js/components/AmountBox.vue
+++ resources/js/components/AmountBox.vue
@@ -2,27 +2,35 @@
     <div class="swizard__years__items">
         <p>{{ label }}</p>
         <div class="swizard__years__items__item">
-        <span class="swizard__years__items__item__left-icon">£</span>
-        <input
-            type="number"
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
-            class="swizard__years__items__item__box hide-arrows"
-        />
-        <span class="swizard__years__items__item__right-icon" 
-            ><IconInfo @mouseover="$emit('icon:click')"
-        /></span>
+            <span class="swizard__years__items__item__left-icon">£</span>
+            <input
+                type="number"
+                :value="modelValue"
+                @input="$emit('update:modelValue', $event.target.value)"
+                class="swizard__years__items__item__box hide-arrows"
+            />
+            <span class="swizard__years__items__item__right-icon">
+                <div class="tooltip">
+                    <IconInfo />
+                    <div class="top">
+                        <p>{{ tooltip }}</p>
+                        <i></i>
+                    </div>
+                </div>
+            </span>
         </div>
     </div>
 </template>
 <script setup>
-
-
 const props = defineProps({
-  label: {
-    type: String,
-    default: "Question"
-  },
-  modelValue: String,
+    label: {
+        type: String,
+        default: "Question",
+    },
+    tooltip: {
+        type: String,
+        default: "",
+    },
+    modelValue: String,
 });
 </script>
