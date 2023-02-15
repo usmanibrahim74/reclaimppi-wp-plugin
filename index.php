25d1e3deb830fa8e54ab42b1ff32253f6716480b
usmanibrahim <usmanibrahim74@gmail.com>
2023-02-15 12:55:42 +0500

    cors

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
 * Author URI: http://synergidigital.com
 **/
 
+function just_add_cors_http_header($headers){
+
+    $headers['Access-Control-Allow-Origin'] = '*';
+
+    return $headers;
+
+}
+
+add_filter('wp_headers','just_add_cors_http_header');
+
+
 
 include_once('wp/constants.php');
 include_once('wp/helpers.php');
