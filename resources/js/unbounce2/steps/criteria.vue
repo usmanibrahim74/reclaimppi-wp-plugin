6865c36e2cf6bd5a6d47e733777c10662d33b070
usmanibrahim <usmanibrahim74@gmail.com>
2023-03-27 17:56:49 +0500

    client changes

--- resources/js/unbounce2/steps/criteria.vue
+++ resources/js/unbounce2/steps/criteria.vue
@@ -1,72 +1,45 @@
 <template lang="">
     <step-title title="Eligibility" />
     <div class="mt-6 mb-4">
-        <template v-if="!state.proceed">
-            <question
-                labelFor=""
-                class="text-center max-w-[450px] mx-auto"
-                text="To see if you are eligible for a refund, please start by clicking proceed."
-            />
-            <!-- text="I was NOT self-employed the year I received my PPI refund." -->
-            <div
-                class="flex flex-col md:flex-row gap-5 justify-center mt-6 items-center"
+        <question
+            labelFor=""
+            class="text-center max-w-[450px] mx-auto"
+            text="What was your employment status at the time of your PPI refund?"
+        />
+        <!-- text="I was NOT self-employed the year I received my PPI refund." -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 mt-6 w-fit mx-auto">
+            <button
+                @click="passed"
+                type="button"
+                class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
             >
-                <button
-                    @click="state.proceed = true"
-                    type="button"
-                    class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
-                >
-                    Proceed
-                </button>
-            </div>
-        </template>
-        <template v-else>
-            <question
-                labelFor=""
-                class="text-center max-w-[450px] mx-auto"
-                text="Please choose an employment status from the options below."
-            />
-            <!-- text="I was NOT self-employed the year I received my PPI refund." -->
-            <div
-                class="grid grid-cols-1 md:grid-cols-2 gap-5 mt-6 w-fit mx-auto"
+                Employed
+            </button>
+
+            <button
+                type="button"
+                class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
+                @click="failed"
             >
-                <button
-                    @click="passed"
-                    type="button"
-                    class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
-                >
-                    Employed
-                </button>
+                Self-Employed
+            </button>
 
-                <button
-                    type="button"
-                    class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
-                    @click="failed"
-                >
-                    Self-Employed
-                </button>
+            <button
+                @click="passed"
+                type="button"
+                class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
+            >
+                Unemployed
+            </button>
 
-                <button
-                    @click="passed"
-                    type="button"
-                    class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
-                >
-                    Unemployed
-                </button>
-
-                <button
-                    @click="passed"
-                    type="button"
-                    class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
-                >
-                    Retired
-                </button>
-            </div>
-            <div class="flex justify-center mt-4">
-
-            <BackButton text="Back" @click="state.proceed = false" />
-            </div>
-        </template>
+            <button
+                @click="passed"
+                type="button"
+                class="border-red-600 border-solid border-2 font-medium text-red-600 flex items-center justify-center rounded py-2 px-4 max-w-[200px] w-full"
+            >
+                Retired
+            </button>
+        </div>
     </div>
 </template>
 <script setup>
@@ -78,7 +51,6 @@ const emitter = inject("emitter");
 const { filterEmptyKeys, arrayIntersection } = Reusable();
 let state = reactive({
     required: ["self_assessment"],
-    proceed: false,
     errors: [],
 });
 const props = defineProps({
